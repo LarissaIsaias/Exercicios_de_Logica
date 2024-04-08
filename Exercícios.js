8a83f20379e3c6073261c90ce2c73e4adce87016
LarissaIsaias <165114332+LarissaIsaias@users.noreply.github.com>
2024-04-08 17:14:32 -0300

    Exercícios de Lógica

---
+++ Exercícios.js
@@ -0,0 +1,93 @@
+//let soma
+//for(let i = 1; i <= 100; i++){
+ //   soma += i
+//}
+//console.log(soma)
+
+
+//let tabuada = "", num
+//num = 5
+//for(let i = 0; i <= 10; i++){
+//tabuada += num + 'x' + i + '=' + (num * i) + '\n'
+//}
+//console.log(tabuada)
+
+
+// let tabuada = "", num
+// num =  Number(prompt('Digite um número: '))
+// for(let i = 0; i <= 10; i++){
+// tabuada += num + 'x' + i + '=' + (num * i) + '\n'
+// }
+// console.log(tabuada)
+
+
+//let decresc = ''
+//for(let i = 10; i >= 1; i--){
+   // decresc += i + '\n'
+//}
+//console.log(decresc)
+
+
+// let num = ''
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 != 0){
+//         num += i + '\n'
+//     }
+// }
+// console.log(num)
+
+
+// let quadrado = ''
+// for(let i = 1; i <= 10; i++){
+//    quadrado += i ** 2 + '\n'
+// }
+// console.log(quadrado)
+
+
+// let cresc = '', decresc = ''
+// for(let i = 1; i <= 10; i++){
+//    cresc += i + '\n'
+// }
+// for(let i = 10; i >= 1; i--){
+//    decresc += i + '\n'
+// }
+// console.log('crescente: ' + '\n' + cresc + '\n' + 'decrescente: ' + '\n' + decresc)
+
+
+// let fatorial = 1
+// for(let i = Number(prompt('Digite um número: ')); i >= 1; i--){
+//    fatorial *= i
+// }
+// console.log(fatorial)
+
+// let numero
+// for (let i = 1; i <= 100; i++){
+//    numero += i + '\n'
+//    if (i % 3 == 0 && i % 5 == 0) {
+//       console.log ('FizzBuzz')
+//    }else if (i % 5 ==0){
+//       console.log('Buzz')
+//    }else if (i % 3 == 0){
+//       console.log('Fizz')
+//    }else{
+//       console.log(i)
+//    }
+// }
+
+
+// let divisao = 0
+// for(let i = 1; i <= 10; i++){
+//    divisao += 1 / i
+// }
+// console.log(divisao)
+
+
+// let par = '', impar = ''
+// for(let i=1; i<=100; i++){
+//    if (i % 2 == 0) {
+//       par++
+//    } else {
+//       impar++
+//    }
+// }
+// console.log('o meu numero de pares é: ' + par + "\n" + 'o meu numero de impares é:' + impar)
